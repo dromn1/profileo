8415f794d2b6cc1135c2dd5474469c8ea0df3ed2
dromn1 <nhamode250@gmail.com>
2023-06-18 19:26:13 +0300

    update

--- components/MainProfileo.js
+++ components/MainProfileo.js
@@ -1,4 +1,5 @@
 import React, {Suspense} from "react";
+import { motion, useAnimation } from "framer-motion"
 import Image from "next/image";
 import avatar from "../images/avatar.svg"
 import snap from "../images/snap.svg"
@@ -9,34 +10,34 @@ export const Model_3d = ()=> {
     return (
         
         <>
-        <div className="col-12">
+        <motion.div  initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:0.2}}  className="col-12">
             <p className="fs-3 fw-bold main-color text-center mt-3 p-0">
                 Profileo
             </p>
-        </div>
+        </motion.div>
         <div className="container ps-4 pe-4">
             <div className="row">
                 <div className="col-12 d-flex justify-content-center p-0 my-2">
-                    <div className="rounded-circle">
+                    <motion.div initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:0.4}}  className="rounded-circle">
                         <Image className="img-fluid" src={avatar} height={90}></Image>
-                    </div>
+                    </motion.div>
                 </div>
-                <div className="col-12 d-flex justify-content-center p-0 my-2">
+                <motion.div initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:0.6}}  className="col-12 d-flex justify-content-center p-0 my-2">
                     <p className="fs-3 fw-bold p-0 m-0">
-                        Ahmad Nasif
+                        Aljouhara Alsaeed
                     </p>
-                </div>
-                <div className="col-12 d-flex justify-content-center">
-                    <p className="fs-6 fw-semibold p-0 m-0">
-                        Product Manger
+                </motion.div>
+                <motion.div initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:0.8}}  className="col-12 d-flex justify-content-center">
+                    <p className="fs-6 fw-semibold text-center p-0 m-0">
+                        Product Manger | @Hungerstation
                     </p>
-                </div>
-                <div className="col-12 d-flex justify-content-center p-0 mb-3 mt-2">
+                </motion.div>
+                <motion.div initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:1}}  className="col-12 d-flex justify-content-center p-0 mb-3 mt-2">
                     <p className="fs-6 text-center p-0 m-0">
-                        jshd sdjkfgsdjh asdkfghjashfv lkasdhfvjshdv jkasdbfvhjads
+                        here some decription or summary about yourself.
                     </p>
-                </div>
-                <div className="col-12 p-0 mb-4 mt-1">
+                </motion.div>
+                {/* <div className="col-12 p-0 mb-4 mt-1">
                     <div className="row d-flex justify-content-between text-center">
                         
                         <div className="col-4 p-0 m-0">
@@ -77,36 +78,36 @@ export const Model_3d = ()=> {
                         </div>
 
                     </div>
-                </div>
+                </div> */}
             </div>
             
         </div>
         <div className="container p-2">
             {/* tabs start */}
             <ul className="nav nav-pills d-flex justify-content-evenly p-0 mb-3 mt-2" id="pills-tab" role="tablist">
-                <li className="nav-item" role="presentation">
-                    <button className="nav-link px-5 main-color active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">QContact</button>
-                </li>
-                <li className="nav-item" role="presentation">
-                    <button className="nav-link px-5 main-color" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">SMedia</button>
-                </li>
+                <motion.li initial={{ x: -50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:1.2}}  className="nav-item" role="presentation">
+                    <button className="nav-link px-5 main-color active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">SMedia</button>
+                </motion.li>
+                <motion.li initial={{ x: 50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:1.4}}  className="nav-item" role="presentation">
+                    <button className="nav-link px-5 main-color" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">QContact</button>
+                </motion.li>
                 </ul>
                 <div className="tab-content" id="pills-tabContent">
                 <div className="tab-pane fade show p-3 text-center active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
                     <div className="row d-flex justify-content-center">
-                        <div className="col-5 p-0 m-0">
+                        <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:1.8}}  className="col-5 p-0 m-0">
                             <Image className="img-fluid my-2" src={twit} height={150}></Image>
-                        </div>
-                        <div className="col-5 p-0 m-0">
+                        </motion.div>
+                        <motion.div  initial={{scale: 0 }} animate={{ scale: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:2}} className="col-5 p-0 m-0">
                             <Image className="img-fluid my-2" src={snap} height={150}></Image>
-                        </div>
+                        </motion.div>
                         <div className="row"></div>
-                        <div className="col-5 p-0 m-0">
+                        <motion.div  initial={{  scale: 0 }} animate={{scale: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:2.2}} className="col-5 p-0 m-0">
                             <Image className="img-fluid my-2" src={insta} height={150}></Image>
-                        </div>
-                        <div className="col-5 p-0 m-0">
+                        </motion.div>
+                        <motion.div  initial={{ scale: 0 }} animate={{scale: 1 }} transition={{type: "spring", stiffness: 260, damping: 20, delay:2.4}} className="col-5 p-0 m-0">
                             <Image className="img-fluid my-2" src={whats} height={150}></Image>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
                 <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">
